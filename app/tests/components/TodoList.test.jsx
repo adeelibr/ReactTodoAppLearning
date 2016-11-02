10153e01f371ad7d520dd75aba76688181768b46
adeelibr <adeelimranr@gmail.com>
2016-11-03 00:29:25 +0500

    TodoList test added to check if todo works

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,22 @@ import expect from 'expect';
 import $ from 'jquery';
 
 import TodoList from 'TodoList';
+import Todo from 'Todo';
 
 describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
+  });
+
+  it('should render one Todo component for each todo item', () => {
+    let todos = [
+      { id: 1, text: 'Do Something' },
+      { id: 2, text: 'Do Something Else' }
+    ];
+
+    let todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    let todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length); 
   });
 });
