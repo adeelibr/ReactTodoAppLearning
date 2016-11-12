50d697f35ca6f68ad4ac76f739f00df22f98dd64
adeelibr <adeelimranr@gmail.com>
2016-11-12 22:34:57 +0500

    should call onAddTodo prop with valid data & should not call onAddTodo prop when invalid input.... tests added to AddTodo.test.jsx

--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -11,12 +11,27 @@ describe('AddTodo', () => {
     expect(AddTodo).toExist();
   });
 
-  if('should call onAddTodo prop with valid data', () => {
+  it('should call onAddTodo prop with valid data', () => {
+    let todoText = 'Check Something'
     let spy = expect.createSpy();
     let addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
     let $el = $(ReactDOM.findDOMNode(addTodo));
-    addTodo.refs.todoText.value = 'Check Something';
+
+    addTodo.refs.newTodo.value = todoText;
     TestUtils.Simulate.submit($el.find('form')[0]);
-    expect(spy).toHaveBeenCalledWith('Check Something');
+
+    expect(spy).toHaveBeenCalledWith(todoText);
+  });
+
+  it('should not call onAddTodo prop when invalid input', () => {
+    let todoText = ''
+    let spy = expect.createSpy();
+    let addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    let $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.newTodo.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled();
   });
 });
